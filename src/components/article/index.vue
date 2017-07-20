fe94a5511d33a6edeca94dec88a5b03b8177f5a3
PZ <pengzhan@mistong.com>
2017-07-20 18:02:35 +0800

    md 解決方案

--- src/components/article/index.vue
+++ src/components/article/index.vue
@@ -1,44 +1,55 @@
 <template>
   <div class="article">
-      <div class="article-title">
-        <h1>小短文</h1>
-        <h2>不定时更新~</h2>
+    <div class="article-title">
+      <h1>小短文</h1>
+      <h2>不定时更新~</h2>
+    </div>
+    <el-card class="box-card">
+      <Post v-highlight class="markdown-body"></Post>
+      <div class="card_footer">
+      <el-button type="primary" class="showall">查看全文</el-button>
       </div>
-      <el-card class="box-card" >
-        <Post v-highlight class="markdown-body"></Post>
-      </el-card>
+  
+    </el-card>
   </div>
 </template>
 <script>
-  import Post from '../../md/test.md'
-  export default{
-    data(){
-      return{
+import Post from '../../md/test.md'
+export default {
+  data() {
+    return {
 
-      }
-    },
-    components:{
-      Post
     }
+  },
+  components: {
+    Post
   }
+}
 </script>
 <style lang="scss" scoped>
-.article{
+.article {
   padding: 50px 100px 0;
-  .article-title{
-    h1{
+  .article-title {
+    h1 {
       text-align: center;
-      color:burlywood;
+      color: burlywood;
       font-size: 36px;
     }
-    h2{
+    h2 {
       text-align: center;
-      color:darkgrey;
+      color: darkgrey;
       font-size: 14px;
       line-height: 40px;
     }
   }
- 
+  .card_footer{
+    display: flex;
+    justify-content: flex-end;
+  }
+  .markdown-body {
+    max-height: 400px;
+    overflow: hidden;
+  }
 }
 </style>
 
